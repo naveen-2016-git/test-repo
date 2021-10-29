2bfabcfb49913c9578c1a1a607622381eb1417f3
naveen-2016-git <naveen.2016.dev@gmail.com>
2021-10-29 13:11:21 +0530

    New v3
    again

--- aws/first_demo/ec2.tf
+++ aws/first_demo/ec2.tf
@@ -1,1 +1,18 @@
 
+resource "aws_instance" "navawsserver" {
+  ami = "ami-02e136e904f3da870"
+  instance_type = "t2.nano"
+
+  tags = {
+    Name = "Naveen-Techlanders-aws-ec2-instance-v3"
+    Env = "Dev"
+  }
+}
+
+output "navawsserver-ip" {
+  value = aws_instance.navawsserver.public_ip
+}
+
+output "myserver-PrivateIP" {
+  value = aws_instance.navawsserver.private_ip
+}
